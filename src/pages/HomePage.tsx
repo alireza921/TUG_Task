f35480a14877b4f7c00aad8aaa94a3a38b7d37f7
AliReza <alirezaesm921@gmail.com>
2025-02-19 18:56:32 +0330

    home page -> lionk to dashboard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,7 @@
+import { Box, Container, Typography } from "@mui/material";
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import { TUGRoutes } from "../TUGRoutes";
 
 interface HomePageProps
 {
@@ -7,7 +10,23 @@ interface HomePageProps
 
 const HomePage: FunctionComponent<HomePageProps> = () =>
 {
-	return (<> Home Page </>);
+	return (
+		<>
+			<Container>
+				<Box sx={{ textAlign: "center", m: 8 }}>
+					<Typography variant="h1">  This is Home Page   </Typography>
+				</Box>
+				<Box sx={{ textAlign: "center", mt: 8 }}>
+					<Typography variant="h4">
+						for moving to Dashboard Page click link below :
+					</Typography>
+					<Typography sx={{ m: 4 }} variant="h5">
+						<Link to={TUGRoutes.Dashboard} > Dashboard </Link>
+					</Typography>
+				</Box>
+			</Container>
+		</>
+	);
 }
 
 export default HomePage;
